88a7ad165ac3e1029fe4f405742c188b0ab53076
ganeshsaini07 <158082670+ganeshsaini07@users.noreply.github.com>
2024-03-09 09:29:36 +0530

    Create iffi.js

---
+++ iffi.js
@@ -0,0 +1,37 @@
+
+// Immediately Invoked Function Expressions (IIFE)
+
+// function  myf(){
+//     console.log(`this is my Function`);
+// }   
+// myf()
+
+// IIFE
+//  () paranthases lagane se ek block bn janta h and global scope ke poluction , declerations se bachne ke liye IIFE
+// ka use kiya jata 
+// IIFE ke through function ko Immediately Excuate kr skate h 
+// kisi function ko global scope ke poluction , declerations se dur rekh kr immeditely run karvana = IIFE 
+
+// (function  myf(){
+    // named IIFE
+//     console.log(`this is my Function`);
+// })();
+
+// (functions define)(functions call)
+
+// ( ()=>{
+//     console.log(`This is my another function`);
+// } )();
+
+// ((name)=>{
+//     console.log(`my name is ${name}`);
+// }) ('ganesh')
+
+
+
+
+
+// ((a)=>{
+//     console.log(`my name is ${a}`);
+// })('ganesh')
+
